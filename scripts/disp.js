81a6305fc54ea0468d20a3b5b7dd3e1646eab43b
liuxilu <liuxilu@live.com>
2021-03-20 21:50:11 +0800

    fix recursive; take 0-255 back

--- scripts/disp.js
+++ scripts/disp.js
@@ -1,95 +1,108 @@
-const padCf = val => {
+const padColor = val => {
 	const str = val.toString(16);
 	return "0".repeat(6 - str.length) + str.toUpperCase();
 };
 
 global.override.block(LogicDisplay, {
-	ldbColour: Color.valueOf("#565656"),
-	sliders: {},
+	ldbColor: { c : 0x565666, r : 0x56, g : 0x56, b : 0x66 },
 	fields: {},
-	// locks to prevent endless loop
-	ldbSetAll : false,
-	ldbSetChannel : false,
+	sliders: {},
+	// locks to prevent recursive setting
+	ldbSetSelf : false,
+	ldbSetOther : false,
+
+	c2rgb(val) {
+		this.ldbColor.c = val;
+		this.ldbColor.r = (this.ldbColor.c >> 16) & 0xFF;
+		this.ldbColor.g = (this.ldbColor.c >>  8) & 0xFF;
+		this.ldbColor.b = (this.ldbColor.c >>  0) & 0xFF;
+		return val;
+	},
+	rgb2c() {
+		const val =
+			((this.ldbColor.r & 0xFF) << 16) +
+			((this.ldbColor.g & 0xFF) <<  8) +
+			((this.ldbColor.b & 0xFF) <<  0);
+		this.ldbColor.c = val;
+		return val;
+	},
 
 	buildConfiguration(table) {
-		const setAll = (val, field) => {
-			if (this.ldbSetAll) return;
-
-			if (!isNaN(val) && 0 <= val && val <= 0xFFFFFF) {
-				this.ldbColour.rgb888(val);
-
-				this.ldbSetAll = true;
-				if (field) {
-					cs.value = val;
-				} else {
-					cf.text = padCf(val);
+		const setAll = (val, setSlider, setField) => {
+			if (!this.ldbSetSelf && !isNaN(val) && 0 <= val && val <= 0xFFFFFF) {
+				this.ldbSetSelf = true;
+				if (setField) {
+					cf.text = padColor(val);
 				}
-				this.ldbSetAll = false;
+				if (setSlider) {
+					cs.value = val;
+				}
+				this.ldbSetSelf = false;
 
-				this.ldbSetChannel = true;
-				setChannel(this.ldbColour.r, "r", true, true);
-				setChannel(this.ldbColour.g, "g", true, true);
-				setChannel(this.ldbColour.b, "b", true, true);
-				this.ldbSetChannel = false;
+				if (!this.ldbSetOther) {
+					this.ldbSetOther = true;
+					this.c2rgb(val);
+					setChannel(this.ldbColor.r, "r", true, true);
+					setChannel(this.ldbColor.g, "g", true, true);
+					setChannel(this.ldbColor.b, "b", true, true);
+					this.ldbSetOther = false;
+				}
 			}
 		};
-
-		const setChannel = (val, chan, slider, field) => {
-			if (!isNaN(val) && 0 <= val && val <= 255) {
-				val /= 255;
-				this.ldbColour[chan] = val;
-
-				this.ldbSetChannel = true;
-				if (slider) {
-					this.sliders[chan].value = val;
-				}
-				if (field) {
+		const setChannel = (val, chan, setSlider, setField) => {
+			if (!this.ldbSetSelf && !isNaN(val) && 0 <= val && val <= 255) {
+				this.ldbSetSelf = true;
+				this.ldbColor[chan] = val;
+				if (setField) {
 					this.fields[chan].text = val;
 				}
-				this.ldbSetChannel = false;
+				if (setSlider) {
+					this.sliders[chan].value = val;
+				}
+				this.ldbSetSelf = false;
 
-				if (!this.ldbSetChannel) {
-					this.ldbSetAll = true;
-					const colour = this.ldbColour.rgb888();
-					cs.value = colour;
-					cf.text = padCf(colour);
-					this.ldbSetAll = false;
+				if (!this.ldbSetOther) {
+					this.ldbSetOther = true;
+					setAll(this.rgb2c(), true, true);
+					this.ldbSetOther = false;
 				}
 			}
 		};
 
 		table.background(Styles.black6);
 
-		global.ldbTipNo("Clear screen",
+		global.ldbTipNo("Clear Screen",
 			table.button(Icon.eraser, Styles.clearTransi, () => {
 				// add "draw clear R G B" to the display's command buffer
-				const r = this.ldbColour.a * 255,
-					g = this.ldbColour.g * 255,
-					b = this.ldbColour.b * 255;
+				const r = this.ldbColor.r,
+					g = this.ldbColor.g,
+					b = this.ldbColor.b;
 				this.commands.addLast(DisplayCmd.get(0, r, g, b, 0, 0, 0));
 			}).size(40).pad(10)
 		);
 
-		const colour = this.ldbColour.rgb888();
-		const cs = table.slider(0, 0xFFFFFF, 1, colour, v => setAll(v, false)).padRight(10).get();
-		const cf = table.field(padCf(colour), v => setAll(parseInt(v, 16), true)).padRight(10).get();
+		const color = this.ldbColor.c;
+		const cs = table.slider(0, 0xFFFFFF, 1, color, v => setAll(v, false, true)).padRight(10).get();
+		const cf = table.field(padColor(color), v => setAll(parseInt(v, 16), true, false)).padRight(10).get();
 		table.row();
 
-		const channel = (colour, chan) => {
-			const value = this.ldbColour[chan];
-			table.add(`[${colour}]${chan.toUpperCase()} `);
+		const channel = color => {
+			const chan = color[0];
+			const value = this.ldbColor[chan];
+			table.add("[" + color + "]" + chan.toUpperCase());
 			this.sliders[chan] = table.slider(0, 0xFF, 1, value, v => setChannel(v, chan, false, true)).padRight(10).get();
-			this.fields[chan] = table.field((value * 255) + "", v => setChannel(parseInt(v), chan, true, false)).padRight(10).get();
+			this.fields[chan] = table.field(value + "", v => setChannel(parseInt(v), chan, true, false)).padRight(10).get();
 			table.row();
 		};
 
-		channel("red", "r");
-		channel("green", "g");
-		channel("blue", "b");
+		channel("red");
+		channel("green");
+		channel("blue");
 
 		table.table(null, t => {
 			const img = new Image();
-			img.update(() => img.color = this.ldbColour);
+			img.color = new Color();
+			img.update(() => img.color.rgb888(this.ldbColor.c));
 			t.add(img).height(50).width(300);
 		}).colspan(3).pad(10);
 	}
